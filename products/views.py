543403c8038f32b821dc8986fc717cf6a14f2aac
nour1029 <nour.m7amad.ezeem@gmail.com>
2022-08-09 22:14:59 +0200

    add product templates [list-detail]

--- products/views.py
+++ products/views.py
@@ -1,3 +1,21 @@
+from msilib.schema import ListView
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Product, ProductImages, Review
+# Creat your views here.
 
-# Create your views here.
+
+class ProductList(ListView):
+    model = Product
+    paginate_by = 1
+
+
+class ProductDetail(DetailView):
+    model = Product
+
+    def get_context_data(self, **kwargs):
+        myproduct = self.get_object()
+        context = super().get_context_data(**kwargs)
+        context["images"] = ProductImages.objects.filter(product=myproduct)
+        context["reviews"] = Review.objects.filter(product=myproduct)
+        return context
