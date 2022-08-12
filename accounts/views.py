185b7aa9060701e15a25ed6a846b01157fddf2ea
nour1029 <nour.m7amad.ezeem@gmail.com>
2022-08-12 20:45:52 +0200

    add sending to gmail

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,8 @@ from multiprocessing import context
 from django.shortcuts import render
 from .forms import SignupForm
 from .models import Profile
+from django.conf import settings
+from django.core.mail import send_mail
 # Create your views here.
 
 
@@ -10,12 +12,20 @@ def signup(request):
         form = SignupForm(request.POST)
         if form.is_valid():
             username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
             myform = form.save(commit=False)
             myform.active = False
             myform.save()
             profile = Profile.objects.get(user__username=username)
             print(profile)
             print(profile.code)
+            send_mail(
+                subject='Activate your account',
+                message=f'use this code {profile.code} to activate your account',
+                from_email='nour.m7amad.ezeem@gmail.com',
+                recipient_list=[email],
+                fail_silently=False,
+            )
     else:
         form = SignupForm()
 
--- project/settings.py
+++ project/settings.py
@@ -138,3 +138,11 @@ MEDIA_ROOT = BASE_DIR / "media"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'nour.m7amad.ezeem@gmail.com'
+EMAIL_HOST_PASSWORD = 'hovovhhccvrdrees'
