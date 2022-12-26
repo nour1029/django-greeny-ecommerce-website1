fb291ea387af1bfb7685c3e1613d7be803488572
nour1029 <nour.m7amad.ezeem@gmail.com>
2022-12-26 17:29:59 +0200

    add get_absolute_url for products models

--- products/models.py
+++ products/models.py
@@ -6,6 +6,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 from django.utils.text import slugify
 from taggit.managers import TaggableManager
 from django.db.models import Avg
+from django.urls import reverse
 # Create your models here.
 
 
@@ -34,10 +35,14 @@ class Product(models.Model):
     class Meta:
         ordering = ('-pk',)
 
-
     def get_avg_reviews(self):
         avg = self.product_review.aggregate(myavg=Avg('rate'))
         return avg
+
+    def get_absolute_url(self):
+        return reverse("products:product_detail", kwargs={"slug":self.slug})
+
+
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
@@ -58,9 +63,11 @@ class ProductImages(models.Model):
 
 class Brand(models.Model):
     name = models.CharField(_("Name"), max_length=50)
-    image = models.ImageField(
-        _("Image"), upload_to='brand/', null=True, blank=True)
+    image = models.ImageField(_("Image"), upload_to='brand/', null=True, blank=True)
     slug = models.SlugField(_("Slug"), blank=True)
+
+    def get_absolute_url(self):
+        return reverse("products:brand_detail", kwargs={"slug":self.slug})
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
@@ -73,6 +80,9 @@ class Brand(models.Model):
 class Category(models.Model):
     name = models.CharField(_("Name"), max_length=50)
     image = models.ImageField(_("Image"), upload_to='category')
+
+    def get_absolute_url(self):
+        return reverse("products:category_detail", kwargs={"slug":self.slug})
 
     def __str__(self):
         return self.name
