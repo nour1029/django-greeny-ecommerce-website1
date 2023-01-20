40bc20878427ecdfa95e106a956203cc3ee1d66a
nour1029 <nour.m7amad.ezeem@gmail.com>
2023-01-20 17:34:12 +0200

    add title_ar field into banner to translate

--- home/models.py
+++ home/models.py
@@ -4,10 +4,11 @@ from django.utils.translation import gettext as _
 
 
 class Banner(models.Model):
-    title = models.CharField(_("Title"), max_length=50)
+    title_en = models.CharField(_("English Title"), max_length=50)
+    title_ar = models.CharField(_("Arabic Title"), max_length=50)
     sub_title = models.CharField(_("Sub Title"), max_length=150)
     image = models.ImageField(_("Image"), upload_to='banner/')
     active = models.BooleanField(_("Active"), default=False)
 
     def __str__(self):
-        return self.title
+        return self.title_eng
