ab3474deb8a6410a1c0c62183381ed6ac4bc5951
nour1029 <nour1029@outlook.sa>
2023-01-22 22:08:40 +0200

    create unauthenticated_user decorator

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from .views import signup, profile, edit_profile, add_profile_number, add_profile_address, edit_profile_number, user_activate, wishlist, delete_profile_contact, delete_profile_address, edit_profile_address, edit_profile_info
-
+from django.contrib.auth.views import LoginView
 
 app_name = 'accounts'
 
 
 urlpatterns = [
+    path('login/', LoginView.as_view(redirect_authenticated_user=True), name="login"),
     path('signup/', signup, name="signup"),
     path('profile/', profile, name="profile"),
     path('profile/edit',  edit_profile_info, name="edit_profile_info"),
