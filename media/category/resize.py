f0cc3293b78386fb5d788e7160522c488ae4c3f8
nour1029 <nour.m7amad.ezeem@gmail.com>
2023-02-20 13:26:34 +0200

    update some products photos

--- media/category/resize.py
+++ media/category/resize.py
@@ -1,7 +1,13 @@
 from PIL import Image
 
+def resize_all_folder(count):
+    for i in range(count):
+        image = Image.open(f"({i+1}).jpg")
+        resized_img = image.resize((250, 120))
+        resized_img.save(f"({i+1}).jpg")
 
-for i in range(10):
-    image = Image.open(f"({i+1}).jpg")
-    resized_img = image.resize((250, 120))
-    resized_img.save(f"({i+1}).jpg")
+
+
+image = Image.open(f"matebook-d-16-list.png")
+resized_img = image.resize((250, 120))
+resized_img.save(f"matebook-d-16-list-resized.jpg")
